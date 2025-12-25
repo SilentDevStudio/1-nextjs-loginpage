27340596d1c0f65e91b477c190f68e5e0b90195c
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2025-12-25 12:19:06 -0300

    Finish: Login Page Project

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,159 @@
+"use client";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+// 1. Setting up the validation schema with zod
+const loginSchema = z.object({
+  email: z.string().email("Type a valid email"),
+  password: z.string().min(6, "Password must have at least 6 characters"),
+});
+
+// Extracting the Type from the Schema
+type LoginFormInputs = z.infer<typeof loginSchema>;
 
 export default function Home() {
+  //2. Setting up the Hook form
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormInputs>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  // Simulating an API call (e.g., 2 seconds wait)
+  const onSubmit = async (data: LoginFormInputs) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log("Loggedin successfully: ", data);
+    alert("Logged in succesfully");
+    reset();
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
+      <div className="flex w-full max-w-7xl overflow-hidden rounded-2xl bg-white shadow-2xl h-175">
+        <div className="w-2/3 relative hidden md:block">
+          <Image
+            src="/login-screen-image.jpg"
+            alt="Login image"
+            fill
+            className="object-cover"
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="flex flex-col w-full lg:w-1/3 justify-between px-8 pt-12">
+          <div className="mx-auto w-full">
+            <div className="mb-12 flex items-center gap-2 text-xl font-semibold">
+              <span className="text-gray-800">{"{ }"}</span>
+              <span className="antialised text-gray-800">
+                Silent Dev Studio
+              </span>
+            </div>
+
+            <h1 className="antialiased tracking-tight mb-6 text-2xl font-medium text-gray-900">
+              Nice to see you again
+            </h1>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+              <div>
+                <label htmlFor="" className="text-gray-500">
+                  Email
+                </label>
+                <input
+                  {...register("email")}
+                  type="email"
+                  placeholder="Email"
+                  className={`w-full bg-gray-100 p-3 text-sm text-gray-600 outline-none transition focus:ring-2 ${
+                    errors.email
+                      ? "focus:ring-red-400 border border-red-400"
+                      : "focus:ring-blue-400"
+                  }`}
+                />
+                {errors.email && (
+                  <p className="mt-1 p-1 bg-red-100 rounded text-xs font-medium text-red-500">
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+
+              <div>
+                <label htmlFor="" className="text-gray-500">
+                  Password
+                </label>
+                <input
+                  {...register("password")}
+                  type="password"
+                  placeholder="Password"
+                  className={`w-full bg-gray-100 p-3 text-sm text-gray-600 outline-none transition focus:ring-2 ${
+                    errors.password
+                      ? "focus:ring-red-400 border border-red-400"
+                      : "focus:ring-blue-400"
+                  }`}
+                />
+                {errors.password && (
+                  <p className="mt-1 p-1 bg-red-100 rounded text-xs font-medium text-red-500">
+                    {errors.password.message}
+                  </p>
+                )}
+              </div>
+
+              <div className="text-right">
+                <a
+                  href="$"
+                  className="text-xs text-blue-600 hover:underline transition"
+                >
+                  Forgot Password?
+                </a>
+              </div>
+
+              <button
+                type="submit"
+                className="flex w-full items-center justify-center gap-3 mb-0 rounded-md bg-blue-600 py-3 font-medium text-white transition hover:bg-blue-700 cursor-pointer disabled:bg-blue-400 disabled:cursor-not-allowed"
+              >
+                {
+                  isSubmitting ? (
+                    <>
+                      <Image src="/loading.svg" alt="Loading" className="h-5 w-5 animate-spin" width={20} height={20} />
+                      <span className="antialiased">Submitting...</span>
+                    </>
+                  ) : (
+                    "Sign In"
+                  )
+                }
+              </button>
+
+              <div className="my-6 border-t border-gray-200"></div>
+
+              <button
+                type="button"
+                className="flex w-full items-center justify-center gap-2 rounded-md bg-zinc-800 py-3 font-medium text-sm text-white transition hover:bg-zinc-900 cursor-pointer"
+              >
+                <Image
+                  src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+                  width={100}
+                  height={100}
+                  alt="Google"
+                  className="h-4 w-4"
+                />
+                Or sign in with Google
+              </button>
+            </form>
+
+            <p className="mt-8 text-center text-sm text-gray-500">
+              Don´t have an account? {""}
+              <a
+                href="#"
+                className="font-medium text-blue-600 hover:underline transition"
+              >
+                Sign up now
+              </a>
+            </p>
+          </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
